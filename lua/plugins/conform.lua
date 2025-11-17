871632ed3bb7eafd7109c9363045d787a7021542
s3nn1k <s3nn1k@gmail.com>
2025-11-17 14:45:12 +0300

    add conform toggle

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -12,18 +12,27 @@ return {
 				yml = { "prettier" },
 				toml = { "taplo" },
 				sql = { "sql_formatter" },
-				-- python = { "black" },
-				-- javascript = { "prettier" },
-				-- typescript = { "prettier" },
-				-- javascriptreact = { "prettier" },
-				-- typescriptreact = { "prettier" },
-				-- css = { "prettier" },
-				-- html = { "prettier" },
 			},
-			format_on_save = {
-				lsp_format = "fallback",
-				timeout_ms = 1250,
-			},
+
+			-- IMPORTANT: Make format_on_save conditional
+			format_on_save = function()
+				if vim.b.disable_autoformat then
+					return nil -- disable formatting
+				end
+				return {
+					lsp_format = "fallback",
+					timeout_ms = 1250,
+				}
+			end,
 		})
+
+		-- Toggle command
+		vim.api.nvim_create_user_command("FormatToggle", function()
+			vim.b.disable_autoformat = not vim.b.disable_autoformat
+			print("Autoformat: " .. (vim.b.disable_autoformat and "OFF" or "ON"))
+		end, {})
+
+		-- Keybind
+		vim.keymap.set("n", "<leader>tf", "<cmd>FormatToggle<CR>", { noremap = true, silent = true })
 	end,
 }
