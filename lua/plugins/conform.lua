1739f4ee4a607e5d3088d2ac4c5e3354ab933b39
s3nn1k <s3nn1k@gmail.com>
2026-08-19 12:24:22 +0300

    update conform for anywhere formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -11,7 +11,7 @@ return {
 				typescript = { "prettier" },
 				typescriptreact = { "prettier" },
 				json = { "prettier" },
-				markdown = { "prettier", "injected" },
+				markdown = { "prettier_md", "injected" },
 				yaml = { "prettier" },
 				yml = { "prettier" },
 				toml = { "taplo" },
@@ -23,6 +23,35 @@ return {
 			},
 
 			formatters = {
+				-- Канон md-text, фаза 1 - проза: жадный перенос к 90; код-блоки prettier не
+				-- трогает (их форматирует injected ниже) - иначе дефолтный prettier влезает в
+				-- примеры (jsonc-запятые, отступы).
+				prettier_md = {
+					command = "prettier",
+					args = {
+						"--stdin-filepath",
+						"$FILENAME",
+						"--prose-wrap",
+						"always",
+						"--print-width",
+						"90",
+						"--embedded-language-formatting",
+						"off",
+					},
+				},
+				-- Канон md-text, фаза 2 (injected): json/jsonc-блоки - форматировать, но без
+				-- trailing-запятых (parser jsonc держит и JSON, и комментарии) и шириной 100.
+				prettier_jsonc = {
+					command = "prettier",
+					args = {
+						"--parser",
+						"jsonc",
+						"--trailing-comma",
+						"none",
+						"--print-width",
+						"100",
+					},
+				},
 				sql_formatter = {
 					args = {
 						"--language",
@@ -33,18 +62,32 @@ return {
 				},
 				injected = {
 					options = {
+						-- битый блок валит форматтеров громко (:ConformInfo / conform.log);
+						-- чинится правкой блока и пересохранением
 						ignore_errors = false,
 						lang_to_ext = { sql = "sql" },
-						lang_to_formatters = { go = { "gofmt" } },
+						lang_to_formatters = {
+							go = { "gofmt" },
+							sql = { "sql_formatter" },
+							json = { "prettier_jsonc" },
+							jsonc = { "prettier_jsonc" },
+						},
 					},
 				},
 			},
 
 			-- IMPORTANT: Make format_on_save conditional
-			format_on_save = function()
-				if vim.b.disable_autoformat then
+			format_on_save = function(bufnr)
+				if vim.b[bufnr].disable_autoformat then
 					return nil -- disable formatting
 				end
+				-- injected-форматтеру нужны готовые treesitter-диапазоны; сразу после старта
+				-- (headless-батч, первое открытие) parse может не успеть - ждём. В
+				-- интерактиве парсер уже готов, выход мгновенный.
+				vim.wait(2000, function()
+					local ok, parser = pcall(vim.treesitter.get_parser, bufnr)
+					return ok and parser ~= nil and #parser:parse() > 0
+				end)
 				return {
 					lsp_format = "never",
 					timeout_ms = 3000,
